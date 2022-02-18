80c97f57cef3438b8ba88db25d2472c278586bf2
张少鹏 <echo996@foxmail.com>
2022-02-18 16:53:55 +0800

    day4 第四天学习方法和接口

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "GoDemo/src"
+import (
+	"GoDemo/src/Day4"
+)
 
 func main() {
 	//src.Day1Start()
 	//src.Day2Start()
-	src.Day3Start()
+	//src.Day3Start()
+	Day4.Day4Start()
 }
