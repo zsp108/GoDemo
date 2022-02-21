5ed17e9421747b6fa17f4c330eb8aa6436e49060
张少鹏 <echo996@foxmail.com>
2022-02-21 19:17:34 +0800

    day4 update

---
+++ src/Day5/day5main.go
@@ -0,0 +1,7 @@
+package Day5
+
+import "fmt"
+
+func Day5Start() {
+	fmt.Println("1")
+}
--- src/day5.go
+++ src/day5.go
@@ -4,8 +4,5 @@ import "fmt"
 
 func Day5Start() {
 	fmt.Println("第五天学习并发")
-	mypointers() // 指针学习调用
-	mystruct()   // 结构体学习调用
-	myarray()    // 数组学习调用
-	myslices()   // 切片学习调用
+
 }
