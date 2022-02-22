853f60992c33751e53fcbeebe338e7b413942a49
张少鹏 <echo996@foxmail.com>
2022-02-22 09:34:14 +0800

    day5 update

--- src/Day5/goroutine.go
+++ src/Day5/goroutine.go
@@ -11,6 +11,10 @@ func say(s string) {
 		fmt.Println(s)
 	}
 }
+
+//goroutine 是由go运行时管理的轻量级线程，调用模式为：go funcname(x,y,x),启动的时候会启动一个新线程并执行
+//而我们普通调用func(x,y,z) 则是发生在当前go线程内的，
+
 func Startgoroutine() {
 	go say("Hello")
 	say("world")
