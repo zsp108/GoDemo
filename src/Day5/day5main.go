affcb36550e47a57e9120c9d2fe8ccb44bed8072
张少鹏 <echo996@foxmail.com>
2022-02-28 19:37:03 +0800

    day5 channel update

--- src/Day5/day5main.go
+++ src/Day5/day5main.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func Day5Start() {
 	fmt.Println("第五天学习并发")
-	//Startgoroutine()
-	fmt.Println("Go channel通道学习")
-	ChannelMain()
-	fmt.Println("带缓冲的channel通道")
-	Bcmain()
-	fmt.Println("通道的range 和close")
-	Racmain()
+	////Startgoroutine()
+	//fmt.Println("Go channel通道学习")
+	//ChannelMain()
+	//fmt.Println("带缓冲的channel通道")
+	//Bcmain()
+	//fmt.Println("通道的range 和close")
+	//Racmain()
 	//RacStart()
+	fmt.Println("select 操作")
+	Smain()
 }
