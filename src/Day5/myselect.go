72360b17c4c9c2d3ed0a19b0f8b135d3d5daa191
张少鹏 <echo996@foxmail.com>
2022-03-01 14:47:46 +0800

    day5 select update

--- src/Day5/myselect.go
+++ src/Day5/myselect.go
@@ -1,6 +1,9 @@
 package Day5
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func myfibonacci(c, quit chan int) {
 	x, y := 0, 1
@@ -19,14 +22,45 @@ func myfibonacci(c, quit chan int) {
 //多个分支准备好时，会随机选择一个执行，如果有default子句，则执行该语句，如果没有则select 会阻塞到某个case可执行
 
 func Smain() {
-	c := make(chan int)
-	quit := make(chan int)
-	go func() {
+	//c := make(chan int)
+	//quit := make(chan int)
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		fmt.Println(<-c)
+	//	}
+	//	quit <- 0
+	//}()
+	//
+	//myfibonacci(c, quit)
+
+	intChan := make(chan int, 10)
+	func() {
 		for i := 0; i < 10; i++ {
-			fmt.Println(<-c)
+			intChan <- i
 		}
-		quit <- 0
 	}()
 
-	myfibonacci(c, quit)
+	strChan := make(chan string, 5)
+	func() {
+		for j := 0; j < 5; j++ {
+			strChan <- "hello " + fmt.Sprintf("%d", j)
+		}
+	}()
+
+	for {
+		select {
+		case v := <-intChan:
+			fmt.Printf("从intChan读取的数据-%d\n", v)
+			time.Sleep(time.Second)
+		case v := <-strChan:
+			fmt.Printf("从stringChan读取的数据%v\n", v)
+			time.Sleep(time.Second)
+		default:
+			fmt.Println("没有数据可取")
+			time.Sleep(time.Second)
+			return
+
+		}
+	}
+
 }
